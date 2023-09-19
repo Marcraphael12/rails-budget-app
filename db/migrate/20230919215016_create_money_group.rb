8f635b9218a034348c2b8148aba09efd1d1f797b
Marc raphael <marcraphael979@gmail.com>
2023-09-19 23:03:25 +0100

    db/migrate: Run migrations

---
+++ db/migrate/20230919215016_create_money_group.rb
@@ -0,0 +1,10 @@
+class CreateMoneyGroup < ActiveRecord::Migration[7.0]
+  def change
+    create_table :money_groups do |t|
+      t.references :group, null: false, foreign_key: true
+      t.references :money_track, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
