cb0163b566925daa442db55f85d93b98b837eae5
Marc raphael <marcraphael979@gmail.com>
2023-09-23 20:27:32 +0100

    Update models
    - add Ability for user
    - update validations
    - update relations
    - update methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :groups
-  has_many :money_tracks
+
+  has_many :groups, dependent: :destroy
+  has_many :money_tracks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { maximum: 30 }
 end
