0424f25f0f37c85eeccf2e0824c6d9db486bfca0
Marc raphael <marcraphael979@gmail.com>
2023-09-24 00:54:29 +0100

    Fix linters

--- app/helpers/money_tracks_helper.rb
+++ app/helpers/money_tracks_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module MoneyTracksHelper
   def group_id
     params.dig(:money_track, :group_id) || params[:group_id]
--- spec/features/money_track/new.html.erb_spec.rb
+++ spec/features/money_track/new.html.erb_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'MoneyTrack view', type: :feature do
